17c09068a0e462976ced7e4f394e11d25592ae90
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-06-07 16:58:36 +0100

    Fix: Set settings to allow collectstatic

--- src/mcare_backend/mcare_backend/settings.py
+++ src/mcare_backend/mcare_backend/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 import dj_database_url
 
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'mcare_backend.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mcare_backend.settings'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,8 +122,7 @@ DATABASES = {
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
@@ -171,8 +170,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # (where they will be served from)
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+)
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
